6a6df3de7a8b18c6283d85af9db284c0a0c71182
Freddy Velarde <freddyvelarde59@gmail.com>
2023-03-08 20:06:31 -0400

    :rocket: UPDATE: docker networking and communication between docker containers is working.

--- pictures/index.js
+++ pictures/index.js
@@ -1,13 +1,25 @@
 import express from "express";
 import morgan from "morgan";
 const app = express();
+// import axios from "axios";
 app.set("port", process.env.PORT || 8000);
 
 app.use(morgan("dev"));
 
-app.get("/", (_req, res) =>
-  res.json({ message: `express server in running on port: ${app.get("port")}` })
-);
+app.get("/", (_req, res) => {
+  // axios
+  //   // .get("https://jsonplaceholder.typicode.com/users")
+  //   .get("http://0.0.0.0:5000")
+  //   .then((response) => {
+  //     res.send(response.data);
+  //   })
+  //   .catch((e) => console.error(e));
+
+  res.json({
+    message: `express server in running on port: ${app.get("port")}`,
+    joke: "yes the docker network is working :)",
+  });
+});
 
 app.get("/all", (_req, res) =>
   res.json({ message: `all users not added yet` })
--- users/app.py
+++ users/app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, jsonify
+import requests
+
 
 app = Flask(__name__)
 PORT = os.environ.get("PORT")
@@ -7,7 +9,14 @@ PORT = os.environ.get("PORT")
 
 @app.route("/")
 def index():
-    return jsonify({"message": "flask server is running on port: " + str(PORT)})
+    #  return jsonify({"message": "flask server is running on port: " + str(PORT)})
+
+    #  response = requests.get("https://jsonplaceholder.typicode.com/users")
+    response = requests.get("http://pictures:8000")
+    #  response = requests.get("http://localhost/api/pictures/")
+    #  print(response.status_code)
+    #  print(response.text)
+    return jsonify(response.json())
 
 
 if __name__ == "__main__":
