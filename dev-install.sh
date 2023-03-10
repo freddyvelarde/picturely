60d4f5d1492f1746c2cc45ab3ad31d9d5ece48ac
Freddy Velarde <freddyvelarde59@gmail.com>
2023-03-09 22:37:13 -0400

    :arrow_up: LIBRARIES: updating dev installer dependencies.

--- dev-install.sh
+++ dev-install.sh
@@ -1,9 +1,11 @@
 # Install "media" express dependencies
+echo "Installing users express dependencies"
 cd ./media/
 pnpm i
 cd ../
 
 # install "users" flask dependencies
+echo "Installing media flask dependencies"
 cd ./users/
 pip install -r requirements.txt
 cd ../
