f207729944d8fb555071fc5974d3cbf51d984911
Freddy Velarde <freddyvelarde59@gmail.com>
2023-03-11 11:23:20 -0400

    :recycle: CONT: golang project integrated in docker containers.

--- posts/main.go
+++ posts/main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Create a new Gin router
 	r := gin.Default()
 
-	// Define a route for handling GET requests to the root path
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to the Posts Services API!",
-		})
+		// Make an HTTP GET request
+		resp, err := http.Get("http://users:5000")
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		defer resp.Body.Close()
+
+		// Read the response body
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		// Set the response content type and body
+		c.Header("Content-Type", "application/json")
+		c.String(http.StatusOK, string(body))
 	})
 
-	// Run the server on port 8080
-	if err := r.Run(":8080"); err != nil {
-		panic(err)
-	}
+	r.Run(":8080")
 }
